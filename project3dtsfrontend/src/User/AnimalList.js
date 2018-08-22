3c6199914224ca57d17fec4b20fa01ab3b0e3b52
Tasha Mitchell <tamitchell14@gmail.com>
2018-08-22 15:36:40 -0400

    one step away from rendering user animal list

---
+++ project3dtsfrontend/src/User/AnimalList.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class AnimalList extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            animal: this.props.data
+        }
+    }
+
+    render() {
+        const animal = this.state.animal
+        return(
+            <div>
+                <h1>{animal.name}</h1>
+                <img src={animal.picture} />
+                <p>{animal.species}</p>
+                <p>{animal.region}</p>
+                <p>{animal.habitat}</p>
+                <p>{animal.endangeredLvl}</p>
+                <p>{animal.population}</p>
+                <div>{animal.about}</div>
+
+            </div>
+        )
+    }
+}
+
+export default AnimalList
--- project3dtsfrontend/src/User/User.js
+++ project3dtsfrontend/src/User/User.js
@@ -1,40 +1,68 @@
-import React, {Component} from 'react'
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import React, { Component } from "react";
+import axios from "axios";
+import AnimalList from "../User/AnimalList";
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from "material-ui/Card";
 
+const url = "http://localhost:3001/user/";
 class User extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
 
-        }
-    }
+  componentDidMount() {
 
-    componentDidMount() {
+    //use this when signup/login works
+    // axios.get(url + this.props.match.params.id)
+      axios
+        .get(url + "5b7d60c42fa6870605b3f9b7")
+        .then(res => {
+          this.setState({
+            user: res.data
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        })
+  }
 
-    }
-
-    render(){
-        return (
-            <div>
-                <div className="user-box">
-                <img src='http://sg-fs.com/wp-content/uploads/2017/08/user-placeholder.png'/>
-                <h5>Username Goes Here</h5>
-                </div>
-                <div className="user-news">
-                    <h3>Latest News</h3>
-                    <ul className="article-container">
-                        <li className="article-item">Article Item</li>
-                    </ul>
-                </div>
-                <div className="user-animallist">
-                    <h3>Animal List</h3>
-                    <ul className="animal-collapsible">
-                        <li className="collapsible-item">Animal Item</li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+  render() {
+      console.log(this.state.user)
+    let showAnimalList = this.state.user.animalList.map(animal => {
+      return (
+        <div key={animal._id}>
+          <AnimalList data={animal} />
+        </div>
+      );
+    });
+    return (
+      <div>
+        <div className="user-box">
+          <img src="http://sg-fs.com/wp-content/uploads/2017/08/user-placeholder.png" />
+          <h5>Username Goes Here</h5>
+        </div>
+        <div className="user-news">
+          <h3>Latest News</h3>
+          <ul className="article-container">
+            <li className="article-item">Article Item</li>
+          </ul>
+        </div>
+        <div className="user-animallist">
+          <h3>Animal List</h3>
+          {showAnimalList}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default User
+export default User;
