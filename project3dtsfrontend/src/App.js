49cebcf9b7a2be863dedff54b33247861400a9c0
DavidZ <david.zafirovic@gmail.com>
2018-08-22 11:36:54 -0400

    It works again

--- project3dtsfrontend/src/App.js
+++ project3dtsfrontend/src/App.js
@@ -3,61 +3,18 @@ import logo from './logo.svg'
 import Header from './Header'
 import Main from './Main'
 import { Route, Link, Switch } from "react-router-dom"
-import axios from 'axios'
-import ShowAnimals from './ShowAnimals'
-import Signup from './Signup'
-import Login from './Login'
+
 class App extends Component {
-  constructor() {
-    super()
 
-    this.state = {
-      email: '',
-      password: '',
-      isLoggedIn: false
-    }
-
-  }
-
-  componentDidMount () {
-    if(localStorage.token) {
-      this.setState({
-        isLoggedin: true
-      })
-    }else {
-      this.setsTATE({
-        isLoggedIn: false
-      })
-    }
-  }
-  handleLogOut(){
-
-  }
-
-  handleInput(e){
-    this.setState({
-      [e.targer.name]: e.target.value
-    })
-  }
-  handleSignUp(e){
-
-  }
-  handleLogIn(e){
-
-  }
 
   render() {
     return (
       <div>
-      <Header isLoggedIn={this.state.isLoggedIn} />
-      <div className = 'body'>
-      <Switch>
-      <Route path = '/signup'
-      render={(props) =>{
-        </div>
-  // <Header/>
-  // <Main/>
 
+
+  <Header/>
+  <Main/>
+</div>
 
 
     );
--- project3dtsfrontend/src/Header.js
+++ project3dtsfrontend/src/Header.js
@@ -1,40 +1,19 @@
-import React, {Component} from 'react'
+
+import React from 'react'
 import {Link} from 'react-router-dom'
-import {Navbar, NavItem} from 'react-materialize'
-import './Header.css'
 
-class Header extends Component {
-  render () {
-    let navBarItems = [<NavItem key={1} href='/'>Animals</NavItem>]
-    if (this.props.isLoggedIn) {
-      navBarItems.push(<NavItem key={2} href='/logout'>Log Out</NavItem>)
-    } else {
-      navBarItems.push(<NavItem key={3} href='/signup'>Sign Up</NavItem>)
-      navBarItems.push(<NavItem key={4} href='/login'>Log In</NavItem>)
-    }
-    return (
-      <Navbar brand='DangerZone' className='nav' right>
-        {navBarItems}
-      </Navbar>
-    )
-  }
-}
-
-
-// const Header = () => (
-//
-//
-//   <header>
-//
-//
-//           <Button><Link to='/'>Home</Link></Button>
-//           <Button><Link to='/signup'>Signup</Link></Button>
-//           <Button><Link to='/login'>Login</Link></Button>
-//           <Button><Link to = '/animals'>Animals</Link></Button>
-//           <Button><Link to = '/donate'>Donate</Link></Button>
-//
-//
-//     </header>
-// )
+const Header = () => (
+  <header>
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/signup'>Signup</Link></li>
+          <li><Link to='/login'>Login</Link></li>
+          <li><Link to = '/animals'>Animals</Link></li>
+          <li><Link to ='/donate'>Donate</Link></li>
+        </ul>
+      </nav>
+    </header>
+)
 
 export default Header
